06aa1353f9905f3da0d8f427b0f2af7b8f6f8eda
mboerwinkle <mboerwinkle@gmail.com>
2016-08-19 13:06:37 +0000

    made it send data to client for bubbles that it is close to

--- user.cpp
+++ user.cpp
@@ -16,23 +16,29 @@ void User::sendUserData(){
 	if(shipList[shipIdx] == NULL){
 		shipIdx = -1;
 	}
+	Ship* myShip = shipList[shipIdx];
+	Ship* targ = shipList[shipIdx];
+	
 	char msg[MSGSIZE] = "SCN";
 	unsigned int msgUsed = 3;
 	memcpy(msg+msgUsed, &tickCount, sizeof(unsigned int));
 	msgUsed+=sizeof(unsigned int);
-	for(int x = 0; x<shipList[shipIdx]->myBubble->shipIdx.len; x++){
-		memcpy(msg+msgUsed, shipList[shipList[shipIdx]->myBubble->shipIdx.list[x]]->pos, sizeof(point));
-		msgUsed+=sizeof(point);
-		memcpy(msg+msgUsed, shipList[shipList[shipIdx]->myBubble->shipIdx.list[x]]->rot, sizeof(quat));
-		msgUsed+=sizeof(quat);
-		if(sizeof(point)+sizeof(quat) + msgUsed >= MSGSIZE){//FIXME speed
-			msg[msgUsed] = 0;
-			msgUsed++;
-			sendto(sockfd, msg, msgUsed, 0, (struct sockaddr*)&(addr), sizeof(addr));
-			char msg[MSGSIZE] = "SCN";
-			int msgUsed = 3;
-			memcpy(msg+msgUsed, &tickCount, sizeof(unsigned int));
-			msgUsed+=sizeof(unsigned int);
+	for(int impShipIdx = 0; impShipIdx < myShip->myBubble->closeImportant.len; impShipIdx++){
+		targ = shipList[myShip->myBubble->closeImportant.list[impShipIdx]];//the ship who has a bubble that we are currently cycling through
+		for(int x = 0; x<targ->myBubble->shipIdx.len; x++){
+			memcpy(msg+msgUsed, shipList[targ->myBubble->shipIdx.list[x]]->pos, sizeof(point));
+			msgUsed+=sizeof(point);
+			memcpy(msg+msgUsed, shipList[targ->myBubble->shipIdx.list[x]]->rot, sizeof(quat));
+			msgUsed+=sizeof(quat);
+			if(sizeof(point)+sizeof(quat) + msgUsed >= MSGSIZE){//FIXME speed
+				msg[msgUsed] = 0;
+				msgUsed++;
+				sendto(sockfd, msg, msgUsed, 0, (struct sockaddr*)&(addr), sizeof(addr));
+				char msg[MSGSIZE] = "SCN";
+				int msgUsed = 3;
+				memcpy(msg+msgUsed, &tickCount, sizeof(unsigned int));
+				msgUsed+=sizeof(unsigned int);
+			}
 		}
 	}
 	if(msgUsed > 3+sizeof(unsigned int)){
