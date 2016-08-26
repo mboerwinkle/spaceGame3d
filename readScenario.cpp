f0e70b0475f442dc076529b0ba36cca7948c6c74
mboerwinkle <mboerwinkle@gmail.com>
2016-08-26 08:31:28 +0000

    added missing files

---
+++ readScenario.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "readScenario.h"
+#include "dataTypes.h"
+#include "ship.h"
+
+void readScenario(char *filename){
+	FILE* fp = fopen(filename, "r");
+	if(fp == NULL){
+		printf("Failed to open scenario \'%s\'\n", filename);
+		return;
+	}
+	int numShips;
+	fscanf(fp, "%d", &numShips);
+	point loc;
+	quat rot;
+	int important;
+	int speed;
+	for(int x = 0; x < numShips; x++){
+		fscanf(fp, "%d %ld %ld %ld %lf %lf %lf %lf %d", &important, &(loc[0]), &(loc[1]), &(loc[2]), &(rot[0]), &(rot[1]), &(rot[2]), &(rot[3]), &speed);
+		new Ship(loc, rot, important, NULL);
+	}
+}
---
+++ readScenario.h
@@ -0,0 +1,1 @@
+extern void readScenario(char *filename);
